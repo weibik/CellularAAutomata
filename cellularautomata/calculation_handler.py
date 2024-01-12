de16291c2aed7f4de3bcb2594a92f163b4a4f949
weibik <j-szkola@wp.pl>
2024-01-13 00:33:53 +0100

    formatted with black

--- cellularautomata/calculation_handler.py
+++ cellularautomata/calculation_handler.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 def set_board(size: int):
     board = np.zeros(size, dtype=int)
-    board[len(board)//2] = 1
+    board[len(board) // 2] = 1
     return board
 
 
 def set_rule(rule_num: int) -> list:
-    binary_representation = format(rule_num, '08b')
+    binary_representation = format(rule_num, "08b")
     result = [int(bit) for bit in binary_representation]
     result.reverse()
     return result
@@ -28,15 +28,14 @@ def evolve(rule_board, initial_state, steps: int):
     for _ in range(steps):
         new_state = np.zeros_like(state)
         for i in range(left_corner, right_corner):
-            print(rule_board)
-            new_state[i] = apply_rule(rule_board, state[i-1], state[i], state[i+1])
+            new_state[i] = apply_rule(rule_board, state[i - 1], state[i], state[i + 1])
         state = new_state.copy()
         history.append(state.copy())
     return history
 
 
 def plot_evolution(history, size, steps):
-    plt.figure(figsize=(size//10, steps//10))
+    plt.figure(figsize=(size // 10, steps // 10))
     plt.imshow(np.array(history), cmap="binary", interpolation="nearest")
     plt.xlabel("Time Step")
     plt.ylabel("Cell")
