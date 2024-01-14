8d4e9f0b9ef83f18a08c8e3ab187ccb0b4587552
weibik <j-szkola@wp.pl>
2024-01-14 12:11:15 +0100

    add docstrings and clean file

--- cellularautomata/calculation_handler.py
+++ cellularautomata/calculation_handler.py
@@ -2,25 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def set_board(size: int):
+def set_board(size: int) -> np.ndarray:
+    """Set 2d board of length size."""
+
     board = np.zeros(size, dtype=int)
     board[len(board) // 2] = 1
     return board
 
 
 def set_rule(rule_num: int) -> list:
+    """Set board with 8 bits based on rule number."""
+
     binary_representation = format(rule_num, "08b")
     result = [int(bit) for bit in binary_representation]
     result.reverse()
     return result
 
 
-def apply_rule(rule_board, left, state, right):
+def apply_rule(
+    rule_board: list, left: np.ndarray, state: np.ndarray, right: np.ndarray
+) -> list:
+    """Apply state of the new cell based on its neighbours."""
+
     binary_value = int(f"{left}{state}{right}", 2)
     return rule_board[binary_value]
 
 
-def evolve(rule_board, initial_state, steps: int):
+def evolve(rule_board: list, initial_state: np.ndarray, steps: int) -> list:
+    """Evolve board 'steps' times."""
+
     left_corner = 1
     right_corner = len(initial_state) - 1
     state = initial_state.copy()
@@ -37,10 +47,12 @@ def evolve(rule_board, initial_state, steps: int):
 
 
 def plot_evolution(history, size, steps):
+    """Plot the evolution image."""
+
     plt.figure(figsize=(size // 10, steps // 10))
     plt.imshow(np.array(history), cmap="binary", interpolation="none", aspect="auto")
     plt.xlabel("Time Step")
     plt.ylabel("Cell")
     plt.title("Cellular Automaton Evolution")
-    plt.get_current_fig_manager().window.state('zoomed')
+    plt.get_current_fig_manager().window.state("zoomed")
     plt.show()
