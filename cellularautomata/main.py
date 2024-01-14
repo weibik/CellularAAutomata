aea05dd1bd1d70180edf38c968a041f09fbc8204
weibik <j-szkola@wp.pl>
2024-01-14 12:35:41 +0100

    add default params

--- cellularautomata/main.py
+++ cellularautomata/main.py
@@ -12,9 +12,9 @@ def start_cellular_automata(board_size, rule, steps):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cellular Automata Simulation")
-    parser.add_argument("board_size", type=int, help="Size of the board.")
-    parser.add_argument("rule", type=int, help="Rule number for cellular automata.")
-    parser.add_argument("num_of_steps", type=int, help="Number of simulation steps.")
+    parser.add_argument("board_size", type=int, default=1501, nargs="?", help="Size of the board.")
+    parser.add_argument("rule", type=int, default=90, nargs="?", help="Rule number for cellular automata.")
+    parser.add_argument("num_of_steps", type=int, default=1000, nargs="?", help="Number of simulation steps.")
 
     args = parser.parse_args()
     start_cellular_automata(args.board_size, args.rule, args.num_of_steps)
